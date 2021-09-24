0cbf039c0bd597ae76ee482212468732f15c591c
TheMorpheus407 <Cedric.Moessner@gmx.de>
2021-09-24 12:53:22 +0200

    Add example

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -2,8 +2,9 @@
 p = starting point coordinates, list of 2D-Tuples
 r = Number of rounds to run the game, integer
 '''
+#Added Example for python main.py --coords 1,0 2,1 0,2 1,2 2,2 --rounds 2204
 def c(p, r):
     print(p)
     print(r)
-    return [(0,0), (1,1), (2,2)]
+    return [(1,0), (2,1), (0,2), (1,2), (2,2)]
     #TO DO
--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # python main.py --coords 1,2 3,4 5,6 --rounds 2
 # python .\main.py --coords 1,2 3,4 5,6 --rounds 2
 
-
 import argparse
 from conways_game_of_life import c
 
@@ -19,7 +18,6 @@ def main():
     parser.add_argument('--coords', help="Coordinates", dest="coords", type=coords, nargs='+')
     parser.add_argument('--rounds', type=int, dest="rounds")
     args = parser.parse_args()
-    print(args)
     tuples = c(args.coords, args.rounds)
     print(tuples) #[(0,0), (1,1), (2,2)]
 
